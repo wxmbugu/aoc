3d0bc3bc043e0d35c87b619a43b333e316b6307b
wambug <wambugumacharia35@gmail.com>
2022-12-03 10:21:26 +0300

    day 1

---
+++ src/events/one.rs
@@ -0,0 +1,24 @@
+use std::{fs::File, io::Read};
+#[allow(unused_must_use)]
+pub fn readfile(file: String) {
+    let mut f = File::open(file).expect("file");
+    let mut s = String::new();
+    f.read_to_string(&mut s);
+    let newstr = s.replace("\n\n", "\ngroup\n");
+    let binding = newstr.replace('\n', ",");
+    let stars: Vec<&str> = binding.split(",group,").collect();
+    let mut largest = 0;
+    for star in stars {
+        let dataset: Vec<i32> = star
+            .trim()
+            .split(',')
+            .filter(|x| !x.is_empty())
+            .map(|x| x.parse().unwrap())
+            .collect();
+        let ok: i32 = dataset.iter().sum();
+        if ok > largest {
+            largest = ok;
+        }
+    }
+    println!("largest = {}", largest)
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+use aoc::one::readfile;
+fn main() {
+    readfile("src/input/d_1/input.txt".to_string());
+}
